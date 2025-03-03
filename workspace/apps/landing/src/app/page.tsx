630614ed82582014fc8d36942e75bc2e630bc3a8
thaufiqumardi-desktop <thaufiqumardii@gmail.com>
2025-03-03 11:32:54 +0700

    update jobs

--- workspace/apps/landing/src/app/page.tsx
+++ workspace/apps/landing/src/app/page.tsx
@@ -1,11 +1,6 @@
 import Typewriter from "./typewritter";
 
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.tailwind file.
-   */
   return (
     <div className="flex flex-col items-center justify-center h-[90vh] sm:h-screen">
       <Typewriter />
